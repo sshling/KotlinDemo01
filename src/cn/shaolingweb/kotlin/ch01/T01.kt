3e91c45f8af344fd147220c668ab07e79346b5b2
shaoling <shaoling@jd.com>
2018-04-18 21:38:23 +0800

    12

--- src/cn/shaolingweb/kotlin/ch01/T01.kt
+++ src/cn/shaolingweb/kotlin/ch01/T01.kt
@@ -2,15 +2,32 @@ package cn.shaolingweb.kotlin.ch01
 
 
 /*
-1. 隐藏类，直观看没有类
-
+0. 隐藏类(T01Kt.class)，直观看没有类
 1. for andorid , type-safety language
 2. less code,less bugs ,less time to coding
-
-
  */
 fun main(args: Array<String>) {
     println("hello world")
+    //分号不是必须
+    //一条语句可跨多行,但字符串,变量名不能跨多行
+    //一行内允许多条语句,分号隔开:  avoid
+
+/*
+硬关键字:
+as --
+as?
+!in
+is
+!is
+object
+typealias
+val
+var
+when
+
+软关键字:
+
+*/
 }
 
 
--- src/cn/shaolingweb/kotlin/ch01/T02.kt
+++ src/cn/shaolingweb/kotlin/ch01/T02.kt
@@ -2,6 +2,35 @@ package cn.shaolingweb.kotlin.ch01
 
 import java.util.function.Consumer
 
+//1. 声明变量
+
+
+//2. 整型:Byte,Short,Int,Long
+
+//3. 浮点型: Float,Double
+
+
+//4 字符型:
+
+
+//5. 数值型之间的类型转换
+
+
+//6. 浮点型与整形之间的转换
+
+
+//7. 表达式类型的自动提升
+
+
+//8. Boolean类型
+
+
+//9. null安全
+
+
+
+
+
 fun main(args: Array<String>) {
     //1. 定义、遍历 List
     val list: List<String> = listOf("hello", "world")
